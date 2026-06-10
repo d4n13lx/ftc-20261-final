1cf984e9e577cadda6e4306041b41ce2bb2ffb1c
d4n13lx <daniel.kazimoto83@gmail.com>
2026-06-10 18:23:26 -0300

    feat(infra): implementando os métodos de criação da Máquina de Turing L4 e teste Unário (Parte 3)

--- ftc-20261-final/Parte3/Infrastructure/ConfiguradorMt.cs
+++ ftc-20261-final/Parte3/Infrastructure/ConfiguradorMt.cs
@@ -11,12 +11,40 @@ namespace ftc_20261_final.Parte3.Infrastructure
         {
             var delta = new Dictionary<(string, char), (string, char, char)>();
 
+            delta[("q0", 'a')] = ("q1", 'X', 'R');
+            delta[("q0", 'Y')] = ("q4", 'Y', 'R');
+
+            delta[("q1", 'a')] = ("q4", 'a', 'R');
+            delta[("q1", 'Y')] = ("q1", 'Y', 'R');
+
+            delta[("q1", 'b')] = ("q2", 'Y', 'R');
+
+            delta[("q2", 'b')] = ("q2", 'b', 'R');
+            delta[("q2", 'Z')] = ("q2", 'Z', 'R');
+
+            delta[("q2", 'c')] = ("q3", 'Z', 'L');
+
+            delta[("q3", 'Z')] = ("q3", 'Z', 'L');
+            delta[("q3", 'Y')] = ("q3", 'Y', 'L');
+            delta[("q3", 'b')] = ("q3", 'b', 'L');
+            delta[("q3", 'a')] = ("q3", 'a', 'L');
+
+            delta[("q3", 'X')] = ("q0", 'X', 'R');
+
+            delta[("q4", 'Y')] = ("q4", 'Y', 'R');
+            delta[("q4", 'Z')] = ("q4", 'Z', 'R');
+
+            delta[("q4", '_')] = ("qaccept", '_', 'R');
+
             return new MaquinaTuring(delta, "q0", "qaccept", "qreject", 1000);
         }
 
         public static MaquinaTuring CriarMtFuncaoUnaria()
         {
             var delta = new Dictionary<(string, char), (string, char, char)>();
+
+            delta[("q0", '1')] = ("q0", '1', 'R');
+            delta[("q0", '_')] = ("qaccept", '1', 'R');
 
             return new MaquinaTuring(delta, "q0", "qaccept", "qreject", 500);
         }
