e85d8f15ddd19d34c39a6a9d7d1b20fe4129e7d7
d4n13lx <daniel.kazimoto83@gmail.com>
2026-06-10 03:16:04 -0300

    feat(infra): implementando o JsonLoader para usar afd.json como parâmetro no modelo

--- ftc-20261-final/Parte1/Infrastructure/AutomacaoJsonLoader.cs
+++ ftc-20261-final/Parte1/Infrastructure/AutomacaoJsonLoader.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace ftc_20261_final.Parte1.Infrastructure
 {
@@ -9,10 +10,48 @@ namespace ftc_20261_final.Parte1.Infrastructure
     {
         public AutomacaoDeterministica CarregarJson(string caminhoArquivo)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(caminhoArquivo))
+            {
+                throw new FileNotFoundException($"Arquivo de configuração não foi encontrado: {caminhoArquivo}");
+            }
+
+            string jsonString = File.ReadAllText(caminhoArquivo);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var dto = JsonSerializer.Deserialize<AutomatoDto>(jsonString, options);
+
+            if (dto == null)
+            {
+                throw new InvalidOperationException("Falha ao mapear o JSON");
+            }
+
+            var transicoesMap = new Dictionary<(string estado, char simbolo), string>();
+            foreach (var t in dto.Delta)
+            {
+                if (t.Simbolo.Length != 1)
+                {
+                    throw new FormatException($"O símbolo de transição '{t.Simbolo}' deve conter 1 caractere apenas");
+                }
+
+                transicoesMap[(t.Origem, t.Simbolo[0])] = t.Destino;
+            }
+
+            return new AutomacaoDeterministica(dto.Q, dto.Sigma, transicoesMap, dto.Q0, dto.F);
         }
 
-        private class AutomatoDto;
-        private class TransicaoDto;
+        private class AutomatoDto
+        {
+            public HashSet<string> Q { get; set; } = new();
+            public HashSet<char> Sigma { get; set; } = new();
+            public List<TransicaoDto> Delta { get; set; } = new();
+            public string Q0 { get; set; } = string.Empty;
+            public HashSet<string> F { get; set; } = new();
+        }
+        private class TransicaoDto
+        {
+            public string Origem { get; set; } = string.Empty;
+            public string Simbolo { get; set; } = string.Empty;
+            public string Destino { get; set;  } = string.Empty;
+        }
     }
 }
