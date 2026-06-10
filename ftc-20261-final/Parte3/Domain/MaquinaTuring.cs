9d013d91fa835ebad027b39d8ef3cdbeec1026c5
d4n13lx <daniel.kazimoto83@gmail.com>
2026-06-10 16:54:57 -0300

    feat(domain):  implementando a lógica da Máquina de Turing (Parte 3)

--- ftc-20261-final/Parte3/Domain/MaquinaTuring.cs
+++ ftc-20261-final/Parte3/Domain/MaquinaTuring.cs
@@ -35,6 +35,51 @@ namespace ftc_20261_final.Parte3.Domain
             var fita = new Dictionary<int, char>();
 
             string entrada = (cadeia == "E") ? "" : cadeia;
+
+            for (int i = 0; i < entrada.Length; i++) 
+            {
+                fita[i] = entrada[i];
+            }
+
+            int cabecote = 0;
+            string estadoAtual = Q0;
+            int contadorPassos = 0;
+
+            Presentation.VisualizadorMt.ImprimirConfiguracao(estadoAtual, fita, cabecote, contadorPassos);
+
+            while (estadoAtual != QAccept && estadoAtual != QReject)
+            {
+                if (contadorPassos >= LimitePassos)
+                {
+                    estadoAtual = QReject;
+                    break;
+                }
+
+
+                char simboloAtual = fita.ContainsKey(cabecote) ? fita[cabecote] : Branco;
+                var chave = (estadoAtual, simboloAtual);
+
+                if (Delta.TryGetValue(chave, out var destino))
+                {
+                    fita[cabecote] = destino.Item2;
+
+                    estadoAtual = destino.Item1;
+
+                    char direcao = destino.Item3;
+
+                    if (direcao == 'R') cabecote++;
+                    else if (direcao == 'L') cabecote--;
+
+                    contadorPassos++;
+                    Presentation.VisualizadorMt.ImprimirConfiguracao(estadoAtual, fita, cabecote, contadorPassos);
+                }
+                else
+                {
+                    estadoAtual = QReject;
+                    Presentation.VisualizadorMt.ImprimirConfiguracao(estadoAtual, fita, cabecote, contadorPassos);
+                }
+            }
+            return (estadoAtual == QAccept, fita, contadorPassos);
         }
     }
 }
