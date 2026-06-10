2fb7532d4fa6388cee0cfcd77817ff85b5d32e48
d4n13lx <daniel.kazimoto83@gmail.com>
2026-06-10 12:09:06 -0300

    feat(domain): implementando lógica em ExplorarCaminhos() (Parte2)

--- ftc-20261-final/Parte2/Domain/AutomatoPilha.cs
+++ ftc-20261-final/Parte2/Domain/AutomatoPilha.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ftc_20261_final.Parte2.Presentation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -43,6 +44,68 @@ namespace ftc_20261_final.Parte2.Domain
 
         public bool ExplorarCaminhos(string estadoAtual, string cadeia, int indice, Stack<char> pilhaCorrente)
         {
+            string cadeiaRestante = cadeia.Substring(indice);
+            VisualizadorAp.ImprimirConfiguracao(estadoAtual, cadeiaRestante, pilhaCorrente);
+
+            if (indice == cadeia.Length && pilhaCorrente.Count == 0)
+            {
+                return true;
+            }
+
+            if (pilhaCorrente.Count == 0)
+            {
+                return false;
+            }
+
+            char topoAtual = pilhaCorrente.Peek();
+
+            var chaveEpsilon = new ChaveTransicao(estadoAtual, Epsilon, topoAtual);
+
+            if (Delta.TryGetValue(chaveEpsilon, out List<DestinoTransicao> destinosEpsilon))
+            {
+                foreach (var destino in destinosEpsilon)
+                {
+                    var pilhaClonada = ClonarPilhaNaoInvertida(pilhaCorrente);
+                    pilhaClonada.Pop();
+
+                    foreach (char c in destino.ElementosEmpilhar.Reverse())
+                    {
+                        pilhaClonada.Push(c);
+                    }
+
+                    if (ExplorarCaminhos(destino.ProximoEstado, cadeia, indice, pilhaClonada))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (indice < cadeia.Length)
+            {
+                char caractereAtual = cadeia[indice];
+
+                var chaveConsumo = new ChaveTransicao(estadoAtual, caractereAtual, topoAtual);
+
+                if (Delta.TryGetValue(chaveConsumo, out List<DestinoTransicao> destinosConsumo))
+                {
+                    foreach (var destino in destinosConsumo)
+                    {
+                        var pilhaClonada = ClonarPilhaNaoInvertida(pilhaCorrente);
+                        pilhaClonada.Pop();
+
+                        foreach (char c in destino.ElementosEmpilhar.Reverse())
+                        {
+                            pilhaClonada.Push(c);
+                        }
+
+                        if (ExplorarCaminhos(destino.ProximoEstado, cadeia, indice + 1, pilhaClonada))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
             return false;
         }
 
