ff7bd9c7dcaf791329383c39ff858d1639925e94
d4n13lx <daniel.kazimoto83@gmail.com>
2026-06-10 14:20:18 -0300

    feat(infra): implementando os métodos de criação do Autômato L1 e L2 (Parte 2)

--- ftc-20261-final/Parte2/Infrastructure/ConfiguradorAutomato.cs
+++ ftc-20261-final/Parte2/Infrastructure/ConfiguradorAutomato.cs
@@ -15,6 +15,31 @@ namespace ftc_20261_final.Parte2.Infrastructure
 
             var delta = new Dictionary<ChaveTransicao, List<DestinoTransicao>>();
 
+            delta[new ChaveTransicao("q0", 'a', 'Z')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "AZ")
+            };
+
+            delta[new ChaveTransicao("q0", 'a', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "AA")
+            };
+
+            delta[new ChaveTransicao("q0", 'b', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q1", "")
+            };
+
+            delta[new ChaveTransicao("q1", 'b', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q1", "")
+            };
+
+            delta[new ChaveTransicao("q1", 'E', 'Z')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q1", "")
+            };
+
             return new AutomatoPilha(estados, sigma, gamma, delta, "q0", 'Z');
         }
 
@@ -25,6 +50,56 @@ namespace ftc_20261_final.Parte2.Infrastructure
             var gamma = new HashSet<char> { 'A', 'B', 'Z' };
 
             var delta = new Dictionary<ChaveTransicao, List<DestinoTransicao>>();
+
+            delta[new ChaveTransicao("q0", 'a', 'Z')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "AZ")
+            };
+            delta[new ChaveTransicao("q0", 'b', 'Z')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "BZ")
+            };
+
+            delta[new ChaveTransicao("q0", 'a', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "AA")
+            };
+            delta[new ChaveTransicao("q0", 'b', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "BA")
+            };
+
+            delta[new ChaveTransicao("q0", 'a', 'B')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "AB")
+            };
+            delta[new ChaveTransicao("q0", 'b', 'B')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q0", "BB")
+            };
+
+            delta[new ChaveTransicao("q0", 'E', 'A')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q1", "A")
+            };
+            delta[new ChaveTransicao("q0", 'E', 'B')] = new List<DestinoTransicao>
+            {
+                new DestinoTransicao("q1", "B")
+            };
+
+
+            delta[new ChaveTransicao("q0", 'a', 'Z')]?.Add(new DestinoTransicao("q1", "Z"));
+            delta[new ChaveTransicao("q0", 'a', 'A')]?.Add(new DestinoTransicao("q1", "A"));
+            delta[new ChaveTransicao("q0", 'a', 'B')]?.Add(new DestinoTransicao("q1", "B"));
+
+            delta[new ChaveTransicao("q0", 'b', 'Z')]?.Add(new DestinoTransicao("q1", "Z"));
+            delta[new ChaveTransicao("q0", 'b', 'A')]?.Add(new DestinoTransicao("q1", "A"));
+            delta[new ChaveTransicao("q0", 'b', 'B')]?.Add(new DestinoTransicao("q1", "B"));
+
+            delta[new ChaveTransicao("q1", 'a', 'A')] = new List<DestinoTransicao> { new DestinoTransicao("q1", "") };
+            delta[new ChaveTransicao("q1", 'b', 'B')] = new List<DestinoTransicao> { new DestinoTransicao("q1", "") };
+
+            delta[new ChaveTransicao("q1", 'E', 'Z')] = new List<DestinoTransicao> { new DestinoTransicao("q1", "") };
 
             return new AutomatoPilha(estados, sigma, gamma, delta, "q0", 'Z');
         }
